c3e352ad0bb5ff3f618bcb59c3e39717ed4e1085
Tyler Rouze <44027587+trouze@users.noreply.github.com>
2021-11-04 21:59:28 -0500

    Cleaned up pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -12,26 +12,13 @@ from prefect.tasks.gcp.bigquery import BigQueryLoadGoogleCloudStorage
 from prefect.tasks.secrets import PrefectSecret
 
 from datetime import timedelta, datetime
-from prefect.schedules import IntervalSchedule
-
-schedule = IntervalSchedule(
-    start_date=datetime.utcnow() + timedelta(seconds=1),
-    interval=timedelta(minutes=1),
-)
-
-# try to pull in the export file and persist it, return it
-# then use it to feed transform data functions which will return pd dataframes
 
 @task
 def extract_health_data(u,p):
     # fetch the export.zip file from iCloud
     # authenticate into iCloud
     api = PyiCloudService(u,p)
-    # grab the health data zip file and extract xml and gpx data
     export = api.drive['Documents']['health_data']['export.zip'].open(stream=True) # get http response object
-    # save zip file locally
-    #z = zipfile.ZipFile(io.BytesIO(export.content))
-    # looks like we'll have to write the zip file and unzip with prefect functions
     file = open("export.zip","wb")
     file.write(export.content)
     file.close()
@@ -51,24 +38,11 @@ def transform_workout_data(nothing):
             li.append(elem.attrib)
             root.clear()
     df = pd.DataFrame(li)
-    # li = []
-    # for _, elem in iterparse(open('apple_health_export/export.xml')):
-    #     if elem.tag == "Workout":
-    #         li.append(elem.attrib)
-    #         elem.clear()
-    # df = pd.DataFrame(li)
     return df
 
 @task
 def transform_gpx_data(nothing):
     # parse .gpx data
-    # change gpxtocsv back to original format which intakes a xml file instead of zip
-    #df = pd.concat([pd.DataFrame(gpxtolist(z,x)) for x in z.namelist() if x.endswith(".gpx")])
-    # for x in os.listdir("apple_health_export/workout-routes"):
-    #     if x.endswith(".gpx"):
-    #         df = pd.concat([pd.DataFrame(gpxtolist(open("apple_health_export/workout-routes/"+x)))])
-    
-    # df = pd.concat([pd.DataFrame(gpxtolist(open("apple_health_export/workout-routes/"+x).read())) for x in os.listdir("apple_health_export/workout-routes") if x.endswith(".gpx")])
     df = pd.concat([pd.DataFrame(gpxtolist("apple_health_export/workout-routes/"+x)) for x in os.listdir("apple_health_export/workout-routes")])
 
     return df
@@ -105,12 +79,7 @@ def push_gpx_data(blob_name, bucket_id, project_id, table_id, dataset_id):
     job.run(write_disposition="WRITE_TRUNCATE", source_format="CSV")
     return
 
-# @task
-# def remove_objs():
-#     return
-
 with Flow("AppleHealth-ETL") as flow:
-    # sets dependencies, it DOES NOT run the functions below
     # set secrets via export PREFECT__CONTEXT__SECRETS__MY_KEY="MY_VALUE"
     project_id = PrefectSecret('PROJECTID')
     bucket_id = PrefectSecret('BUCKETID')
@@ -131,5 +100,5 @@ with Flow("AppleHealth-ETL") as flow:
     # tell BQ data transfer to pick up new data
     push_health_data(workout_blob,bucket_id,project_id,health_tableid,dataset_id)
     push_gpx_data(gpx_blob, bucket_id, project_id, gpx_tableid, dataset_id)
-    #flow.run()
+    
 flow.register(project_name="health-data")
